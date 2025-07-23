0dd57167e8e95d243ea6a2e4e68642709a1d854b
Muhammad Ahmed <ma0621450@gmail.com>
2025-07-24 00:33:05 +0500

    spa script debug

--- resources/views/partials/gtm.blade.php
+++ resources/views/partials/gtm.blade.php
@@ -11,3 +11,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZS9B6DK"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+<script>
+            document.addEventListener('livewire:navigated', () => {
+            console.log("✅ livewire:navigated triggered");
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+                event: "page_view",
+                page_path: window.location.pathname,
+                page_location: window.location.href,
+                page_title: document.title
+            });
+            console.log("✅ GTM page_view pushed to dataLayer");
+        });
+</script>
